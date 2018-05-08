9913b67d49c7bc51726755d4b57df9a68dbf3adf
Gavin Look <GTLook@gmail.com>
2018-05-07 17:52:08 -0700

    Inital commit, created MVC folders, NPM modules, .gitignore, and server.js formating

---
+++ src/routes/snacks.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const dataController = require('../controllers/snacks')
+
+//////////////////////////////////////////////////////////////////////////////
+// Basic CRUD Methods
+//////////////////////////////////////////////////////////////////////////////
+
+router.get('/snacks', dataController.getAll)
+
+module.exports = router
