bfe06e922e38a34ba9e5a87ec64f74a0bd67ad1d
Mark Pavlovski <markpavlovski@gmail.com>
2018-05-29 19:09:56 -0700

    updated the put route to include the review id

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -11,7 +11,7 @@ const authController = require('../controllers/auth')
 router.get('/reviews/', dataController.getAll)
 //router.get('/snacks/:id/reviews/:reviewId', dataController.getOne)
 router.post('/snacks/:id/reviews/', authController.isAuthenticated, dataController.create)
-router.put('/snacks/:id/reviews/', authController.isAuthenticated, dataController.modify)
+router.put('/snacks/:id/reviews/:reviewId', authController.isAuthenticated, dataController.modify)
 router.delete('/snacks/:id/reviews/:reviewId', authController.isAuthenticated, dataController.remove)
 
 module.exports = router
